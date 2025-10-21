c2e6e3ea606af2446aefc11a693e3cca1dec8cd2
willemenezes <willemenezes.jt@gmail.com>
2025-10-21 12:03:00 -0300

    feat(analytics): add Vercel Analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById("root")!).render(
-    <App />
+    <>
+        <App />
+        <Analytics />
+    </>
 );
