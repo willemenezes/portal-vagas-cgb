28bcfe982893bb648f3163d9b3642799f0a766d0
willemenezes <willemenezes.jt@gmail.com>
2025-11-28 10:58:06 -0300

    Corrigir estrutura JSX: fechar div space-y-4 antes do DialogFooter

--- src/components/admin/JobRequestsManagement.tsx
+++ src/components/admin/JobRequestsManagement.tsx
@@ -1633,6 +1633,7 @@ const JobRequestsManagement = () => {
                                 />
                             </div>
                         </div>
+                    </div>
                     <DialogFooter>
                         <Button
                             variant="outline"
